0e00cf689411ffca1d973009ddd919ab68795003
christianchkwuka <145847285+christianchkwuka@users.noreply.github.com>
2026-05-04 18:13:34 +0100

    Create pfsense-wazuh-detection.m

---
+++ 03-Wazuh-Detection/pfsense-wazuh-detection.m
@@ -0,0 +1,9 @@
+## Step 1: Testing the pfSense Custom Detection Rule
+
+### Objective
+The objective of this step was to verify that the custom Wazuh rule for pfSense firewall logs works correctly.
+
+### Command Used
+
+```bash
+sudo /var/ossec/bin/wazuh-logtest
